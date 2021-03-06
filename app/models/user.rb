ea5f2b15b18a1579f8abfa599d39a76ed726fd70
kazu0602 <kazuhona5555@yahoo.co.jp>
2021-03-06 20:45:37 +0900

    新規登録/ユーザ情報/本人情報確認

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,19 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable 
+#end
+PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i
+NAME_REGEX = /\A[ぁ-んァ-ン一-龥]/
+NAME_KANA_REGEX = /\A[ぁ-んァ-ン一-龥]/
+
+validates :nickname, presence: true
+validates :password, format: { with: PASSWORD_REGEX }, presence: true
+validates :email, presence: true, uniqueness: { case_sensitive: false }
+validates :family_name, presence: true, format: { with: NAME_REGEX }
+validates :given_name, presence: true, format: { with: NAME_REGEX }
+validates :family_name_kana, presence: true, format: { with: NAME_KANA_REGEX }
+validates :given_name_kana, presence: true, format: { with: NAME_KANA_REGEX }
+validates :birth, presence: true
+
 end
