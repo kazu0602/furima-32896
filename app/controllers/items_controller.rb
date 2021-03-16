ebd13bd5c7454856830e235ae3b0cf6ad5bc2bc5
kazu0602 <kazuhona5555@yahoo.co.jp>
2021-03-16 22:15:11 +0900

    商品一覧表示

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,6 +2,7 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index]
 
   def index
+    @items = Item.all.order(created_at: :desc)
   end
 
   def new
@@ -17,9 +18,9 @@ class ItemsController < ApplicationController
     end
   end
 
-  # def show
-  #   @item = Item.new
-  # end
+  def show
+    @item = Item.find(params[:id])
+  end
 
 
   #以下はActive Storage導入時に追加したもの
