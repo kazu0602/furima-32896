475a7bc634e3ae678ed9f0b02f6dc5bcc47ea7b2
kazu0602 <kazuhona5555@yahoo.co.jp>
2021-03-19 20:09:42 +0900

    before_actionでまとめる

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :find_item, only: [:edit, :update, :show ]
 
   def index
     @items = Item.all.order(created_at: :desc)
@@ -10,14 +11,12 @@ class ItemsController < ApplicationController
   end
 
   def edit
-    @item = Item.find(params[:id])
     unless @item.user == current_user
       redirect_to root_path
     end
   end
 
   def update
-    @item = Item.find(params[:id])
     if @item.update(item_params)
       redirect_to item_path(@item.id)
    else
@@ -35,20 +34,16 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
 
-  #以下はActive Storage導入時に追加したもの
   private
 
   def item_params
     params.require(:item).permit(:image, :title, :concept, :category_id, :status_id, :fee_id, :prefecture_id, :delivery_date_id, :price).merge(user_id: current_user.id)
   end
 
-  # def message_params
-  #   params.require(:message).permit(:content, :image).merge(user_id: current_user.id)
-  # end
-  #ここまで
-  #保存以降は後ほど実装予定
+  def find_item
+    @item = Item.find(params[:id])
+  end
 end
