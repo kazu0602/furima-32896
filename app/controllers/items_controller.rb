ddb06f2db32be2309075071e0bcd6d65fd45dc50
kazu0602 <kazuhona5555@yahoo.co.jp>
2021-03-19 19:42:11 +0900

    リンクベタ打ち制限

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -11,12 +11,15 @@ class ItemsController < ApplicationController
 
   def edit
     @item = Item.find(params[:id])
+    unless @item.user == current_user
+      redirect_to root_path
+    end
   end
 
   def update
     @item = Item.find(params[:id])
     if @item.update(item_params)
-      redirect_to item_path(item.id)
+      redirect_to item_path(@item.id)
    else
      render :edit
    end
