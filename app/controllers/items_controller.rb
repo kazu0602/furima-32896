df42c8d4fd87c404cf53b4fb205b1ba19896afb8
kazu0602 <kazuhona5555@yahoo.co.jp>
2021-03-20 15:30:32 +0900

    修正3

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
   before_action :find_item, only: [:edit, :update, :show ]
+  before_action :user_authentication, only: [:edit, :update]
 
   def index
     @items = Item.all.order(created_at: :desc)
@@ -11,9 +12,6 @@ class ItemsController < ApplicationController
   end
 
   def edit
-    unless @item.user == current_user
-      redirect_to root_path
-    end
   end
 
   def update
@@ -46,4 +44,11 @@ class ItemsController < ApplicationController
   def find_item
     @item = Item.find(params[:id])
   end
+  
+  def user_authentication
+    unless @item.user == current_user
+      redirect_to root_path
+    end
+  end
+
 end
