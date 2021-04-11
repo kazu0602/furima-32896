98be41da0cf15aa03a54b5371666c5eea98fb85c
kazu0602 <76655702+kazu0602@users.noreply.github.com>
2021-04-11 11:36:33 +0900

    basic認証

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,18 +1,17 @@
 class ApplicationController < ActionController::Base
-  before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :configure_permitted_parameters, if: :devise_controller? 
+  before_action :basic_auth
 
   private
 
   def configure_permitted_parameters
-    # devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname])
-    # devise_parameter_sanitizer.permit(:sign_up, keys: [:encrypted_password])
-    # devise_parameter_sanitizer.permit(:sign_up, keys: [:family_name])
-    # devise_parameter_sanitizer.permit(:sign_up, keys: [:given_name])
-    # devise_parameter_sanitizer.permit(:sign_up, keys: [:family_name_kana])
-    # devise_parameter_sanitizer.permit(:sign_up, keys: [:given_name_kana])
-    # devise_parameter_sanitizer.permit(:sign_up, keys: [:birth])
-    # permitしないとカラムに保存されなくなる
     devise_parameter_sanitizer.permit(:sign_up,
                                       keys: [:nickname, :family_name, :given_name, :family_name_kana, :given_name_kana, :birth])
+  end
+
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == 'kazushi' && password == '1234'
+    end
   end
 end
